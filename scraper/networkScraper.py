8de3b8dd34d3ff0a1408dcf5c3f95f64e0a10d21
dbrd <dbrd@cs.washington.edu>
2025-02-04 16:53:38 -0800

    factored out some of the code for scraping api responses, qfc kinda works, safewya doesnt

---
+++ scraper/networkScraper.py
@@ -0,0 +1,18 @@
+from curl_cffi import requests as cureq
+import pandas as pd
+from config import sites
+
+def get_network_data():
+    for site in sites:
+        url = site['url']
+        headers = site['headers']
+        cookies = site['cookies']
+
+        response = cureq.get(url, headers=headers, cookies=cookies, impersonate='chrome')
+        if response.status_code == 200:
+            df = pd.DataFrame(response.json())
+            df.to_json(site['name'] + '_network_data.json', orient='records', lines=True)
+        else:
+            return None
+
+get_network_data()
