a760cc3ec68525aeac78d99dc4de96af7d750203
Nathaniel Moreland <106217304+NMIV1@users.noreply.github.com>
2025-02-19 22:52:39 -0800

    fixed tests

--- FrugalApp/app/_layout.tsx
+++ FrugalApp/app/_layout.tsx
@@ -1,11 +1,10 @@
 /**
- * 
+ *
  *
  * This file defines the layout for all the screens in the app.
  * To better understand how expo's new router works, check out the documentation:
- * https://docs.expo.dev/router/navigating-pages/
+ * 
  *
- * *** How to use buttons: *** https://docs.expo.dev/router/navigating-pages/#buttons
  */
 
 import { Stack } from "expo-router";
