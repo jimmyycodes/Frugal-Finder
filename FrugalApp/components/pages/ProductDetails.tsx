89fb2ca330395cda427c18f417e599c9d89355df
Nathaniel Moreland <106217304+NMIV1@users.noreply.github.com>
2025-02-19 17:16:46 -0800

    in progress

--- FrugalApp/components/pages/ProductDetails.tsx
+++ FrugalApp/components/pages/ProductDetails.tsx
@@ -5,6 +5,7 @@ import { useNavigation, useRoute, RouteProp } from "@react-navigation/native";
 import { RootStackParamList } from "@/types/navigation";
 import PrimaryButton from "../Buttons/PrimaryButton";
 import LongItem from "../Items/LongItem";
+import { ScrollView } from "react-native-reanimated/lib/typescript/Animated";
 
 type ProductDetailsRouteProp = RouteProp<RootStackParamList, "ProductDetails">;
 
@@ -34,10 +35,15 @@ export default function ProductDetails() {
           <Text style={styles.title}>Organic Lemons</Text>
           <Text style={styles.ammount}>1.50 lbs</Text>
           <StoreList stores={["walmart", "Safeway", "", "", "Trader joes"]} />
-          <Text>Description</Text>
+          <Text style={styles.desc}>
+            {
+              "Organic Mountain works as a seller for many organic growers of organic lemons. Organic lemons are easy to spot in your produce aisle. They are just like regular lemons, but they will usually have a few more scars on the outside of the lemon skin. Organic lemons are considered to be the world's finest lemon for juicing"
+            }
+          </Text>
         </View>
       </View>
 
+      <Quantity />
       <PrimaryButton title="Add to Cart" onPress={() => null} />
       <View style={styles.longItemContainer}>
         <LongItem
@@ -52,6 +58,39 @@ export default function ProductDetails() {
     </View>
   );
 }
+
+function Quantity() {
+
+  return (
+    <View style={QuantStyles.container}>
+      <Text>Quantity</Text>
+    </View>
+  );
+
+}
+
+const QuantStyles = StyleSheet.create({
+
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "#FFFFFF",
+  },
+  button: {
+    backgroundColor: "#F2FFE6",
+    borderRadius: 5,
+    padding: 10,
+  },
+  buttonText: {
+    fontSize: 20,
+  },
+  quantity: {
+    fontSize: 20,
+  },
+
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -73,11 +112,13 @@ const styles = StyleSheet.create({
   imageHeader: {},
   textHeader: {
     padding: 10,
+    paddingBottom: 15,
   },
   favoriteButton: {
     position: "absolute",
     right: 20,
     top: 210,
+    zIndex: 100,
   },
   price: {
     fontSize: 16,
@@ -89,6 +130,11 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   ammount: {
+    color: "#868889",
+    paddingBottom: 10,
+  },
+  desc: {
+    paddingTop: 10,
     color: "#868889",
   },
 });
