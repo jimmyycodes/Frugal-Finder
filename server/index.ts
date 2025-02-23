69aab446682909ec417b31656b536dc3efd05dc5
rgcrawf <roningcrawford@gmail.com>
2025-02-22 23:47:51 -0800

    Change index.ts to .js temporarily, server runs

--- server/index.ts
+++
@@ -1,8 +0,0 @@
-import express, { Express } from "express";
-import bodyParser from 'body-parser';
-
-const port: number = 8088;
-const app: Express = express();
-app.use(bodyParser.json());
-
-app.listen(port, () => console.log('Server listening on ${port}'));
