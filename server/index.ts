5d57ec34841ef037370196dc85bbc8d4f982a3b5
rgcrawf <roningcrawford@gmail.com>
2025-02-24 00:57:21 -0800

    Print all from tjproducts to console

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,61 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import fs, { PathOrFileDescriptor } from "fs";
 
 // might move sql stuff to different file
-import mysql from "mysql";
+import mysql, { ConnectionOptions } from "mysql2";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+// fill out credentials in .env
+const dbConfig: ConnectionOptions = {
+    host: process.env.HOST!,
+    user: process.env.USER!,
+    password: process.env.PASSWORD!,
+    database: process.env.DATABASE!,
+    port: parseInt(process.env.DBPORT!),
+    ssl: {ca: fs.readFileSync(process.env.SSLPATH!)}
+}
+const conn = mysql.createConnection(dbConfig);
+
+conn.connect(
+    function (err) {
+        if (err) {
+            console.log("Can't connect. Error:");
+            throw err;
+        } else {
+            console.log("Connection established.");
+            readData();
+        }
+    }
+)
+
+function readData() {
+    conn.query('SELECT * FROM tjproducts',
+        function (err, results, field) {
+            if (err) {
+                throw err;
+            } else {
+                for (const result in results) {
+                    console.log('Row: ' + JSON.stringify(result));
+                }
+            }
+        }
+    )
+    conn.end(
+        function (err) {
+            if (err) {
+                throw err;
+            } else {
+                console.log('Closing connection.');
+            }
+        }
+    )
+}
+
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
