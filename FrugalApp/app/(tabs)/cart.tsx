366261853b6bfcced434456ff8142df986c353f4
Nathaniel Moreland <106217304+NMIV1@users.noreply.github.com>
2025-03-02 15:31:15 -0800

    removed mock, added longItemGen to cart

--- FrugalApp/app/(tabs)/cart.tsx
+++ FrugalApp/app/(tabs)/cart.tsx
@@ -1,27 +1,18 @@
 import React from "react";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
-import LongItem from "@/components/Items/LongItem";
 import PrimaryButton from "@/components/Buttons/PrimaryButton";
+import { useState } from "react";
+import { mockItems } from "@/constants/MockVars";
+import genLongItems from "@/constants/Tools";
 
 export default function Cart() {
+  const [items, setItems] = useState(genLongItems(mockItems, () => null, () => null, false));
+
   return (
     <View style={styles.container}>
       <ScrollView>
         <View style={styles.scrollCont}>
-          <LongItem
-            name="Extra Large Green Se..."
-            store="Target"
-            image="https://target.scene7.com/is/image/Target/GUEST_27ecaa50-cac2-49f8-9a9b-e59800e28d8a?wid=1200&hei=1200&qlt=80&fmt=webp"
-            amount="1.50 lbs"
-            price={4.29}
-          />
-          <LongItem
-            name="Fresh Green Seedles..."
-            store="Walmart"
-            image="https://i5.walmartimages.com/seo/Fresh-Green-Seedless-Grapes-2-25-lbs-Bag-Est_9b543e57-d12c-4b2f-af70-cbfc8166dce1.19eafb20170233f7df74f7a6c5ff5530.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF"
-            amount="2.25 lbs"
-            price={5.13}
-          />
+          {items}
         </View>
       </ScrollView>
       <View style={styles.totals}>
