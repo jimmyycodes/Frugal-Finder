a57e19b8a91f48faea6e27a1a6f9e8b03bdc7356
jimmyle1 <jimmyle1@cs.washington.edu>
2025-03-06 14:18:24 -0800

    testing fix on yaml for mysql connection to azure

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -29,19 +29,19 @@ def clear_products_table():
     db_connection.close()
 
 def run_tj_scraper():
-    categories = ["bread"]
+    categories = ["Fruits", "Vegetables", "Meat"]
     tj_scraper = TJ_scraper()
     tj_scraper.scrape(categories)
 
 def run_qfc_scraper():
-    categories = ["bread"]
+    categories = ["Fruits", "Vegetables", "Meat"]
     qfc_scraper = QFC_scraper()
     qfc_scraper.scrape(categories)
 
 if __name__ == '__main__':
     # Clear products table
     clear_products_table()
-    
+
     # Create processes
     p1 = Process(target=run_tj_scraper)
     p2 = Process(target=run_qfc_scraper)
