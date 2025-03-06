ebd53f5241016f331c6ccf453b9c5494fac047da
rgcrawf <roningcrawford@gmail.com>
2025-03-05 22:14:31 -0800

    Fix SQL statements for API calls to match new database schema, add getFeatured api call
    Co-authored-by: ITpapers <ITpapers@users.noreply.github.com>

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import { getAll, getCategory } from './routes';
+import { getAll, getCategory, getFeatured } from './routes';
 
 dotenv.config();
 
@@ -9,6 +9,7 @@ const port = process.env.PORT || 3000;
 
 app.get("/api/getAll", getAll);
 app.get("/api/getCategory", getCategory);
+app.get("/api/getFeatured", getFeatured);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
