8cf2844a6d1e39172fd47d1e8036b477ca7033fd
Nathaniel Moreland <106217304+NMIV1@users.noreply.github.com>
2025-03-07 17:03:18 -0800

    added functioning cart

--- FrugalApp/app/(tabs)/heart.tsx
+++ FrugalApp/app/(tabs)/heart.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function Heart() {
   return (
-    <View>
-      <Text>heart</Text>
+    <View style={styles.container}>
+      <Text>Coming Soon</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
