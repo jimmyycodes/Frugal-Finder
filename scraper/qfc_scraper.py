e4501dd4506a7fe36e47e0959081f00ea04aa9ae
jimmyle1 <jimmyle1@cs.washington.edu>
2025-03-07 18:25:59 -0800

    fixed moduel type for scrapers

--- scraper/qfc_scraper.py
+++ scraper/qfc_scraper.py
@@ -1,11 +1,8 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
-
-import undetected_chromedriver as uc
 
 from dotenv import find_dotenv, load_dotenv
 import mysql.connector
